11cb31aee700e2bf0b19c7ab0cad1f0d76d58966
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-04-04 14:02:22 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import logo "/logos-removebg-preview.png"
 
 const Home = () => {
 
   return (
     <div className='container mt-5 p-5  text-light'>
 
-      <center><h2>Welcome to <span className='text-primary'>E</span>lopy<span className='text-warning'>S</span>age AI models</h2></center>
+      <center><h2 className="mt-4">Welcome to <span className='text-primary'>E</span>lopy<span className='text-warning'>S</span>age AI models</h2>
+      <div>
+        <img src={logo} width="200px" height="200px"/>
+      </div>
+      </center>
 
     </div>
   )
